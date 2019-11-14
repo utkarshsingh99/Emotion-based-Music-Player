ff02b737a8d3f154671012dafd03c836f906d0b2
utkarshsingh99 <utkarshsingh369@gmail.com>
2019-11-14 19:12:48 +0530

    Imag Input Working

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for
+# -*- coding: utf-8 -*-
+from flask import *
 import paralleldots
 from werkzeug.utils import secure_filename
 import os
 
 app = Flask(__name__)
-
-UPLOAD_FOLDER = '/'
+app.config['SECRET_KEY'] = 'madmajksgdckua'
+UPLOAD_FOLDER = '/static'
 paralleldots.set_api_key("Kyy830QxC01AsSg9Y4eYFtQo5JYAK6l7zc9jIJ1oNJ8")
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
@@ -19,20 +20,28 @@ def home():
 
 @app.route("/song", methods = ['GET', 'POST'])
 def song():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+	if request.method == 'POST':
+		# Uploading and saving the image file locally
+	    # check if the post request has the file part
+	    if 'file' not in request.files:
+	        flash('No file part')
+	        return redirect(request.url)
+	    file = request.files['file']
+	    # if user does not select file, browser also
+	    # submit a empty part without filename
+	    if file == '':
+	        flash('No selected file')
+	        return redirect(request.url)
+	    if file:
+	    	filename = secure_filename(file.filename)
+	    	print(os.path.abspath(os.path.join(app.config['UPLOAD_FOLDER'], filename)))
+	    	file.save(filename)
+
+	    	# Send the image to the API
+	    	
+	    	return Response(200)
+	else:
+		return Response(500)
 
 if __name__ == "__main__":
     app.run(debug=True)
